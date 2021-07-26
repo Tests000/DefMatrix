42b74de6b563cc2b82d41f9cdbbe111ccbef2aa7
Арутюнян Артур <vrnturchay@gmail.com>
2021-07-26 21:59:50 +0300

    Добавьте файлы проекта.

---
+++ NMatrix/Matrix.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NMatrix
+{
+    class Matrix
+    {
+        double[,] matrix;
+        int size;
+        public Matrix(int a)
+        {
+            size = a;
+            string[] Array;
+            matrix = new double[a, a];
+            for (int i = 0; i < a; i++)
+            {
+                Array = Console.ReadLine().Split(' ');
+                for (int j = 0; j < a; j++)
+                    matrix[i, j] = Int32.Parse(Array[j]);
+            }
+        }
+
+        public double N()
+        {
+            double res = 1;
+            double cof;
+            double rescof = 1;
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = i + 1; j < size; j++)
+                {
+                    cof = matrix[i, i] / matrix[j, i];
+                    for (int k = i; k < size; k++)
+                    {
+                        matrix[j, k] = matrix[i, k] - matrix[j, k] * cof;
+                    }
+                    rescof *= -cof;
+                }
+            }
+            Print();
+            for (int i = 0; i < size; i++)
+            {
+                res *= matrix[i, i];
+            }
+            return res/rescof;
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                    Console.Write(matrix[i, j].ToString() + ' ');
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ NMatrix/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var matrix = new Matrix(3);
+            Console.WriteLine(matrix.N());
+        }
+    }
+}
